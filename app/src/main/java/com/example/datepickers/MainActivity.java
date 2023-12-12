01110c9d3666bd6cc04db8a2b9ed08221f496934
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-12-11 23:12:08 -0700

    2da. forma...

--- app/src/main/java/com/example/datepickers/MainActivity.java
+++ app/src/main/java/com/example/datepickers/MainActivity.java
@@ -1,31 +1,22 @@
 package com.example.datepickers;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.util.Pair;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
-import
-        com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button mDatePickerBtn;
-    private TextView mSelectedDateText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mDatePickerBtn = findViewById(R.id.date_picker_btn);
-        mSelectedDateText = findViewById(R.id.selected_date);
-
         //MaterialDatePicker
-        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
         builder.setTitleText("SELECT A DATE");
         final MaterialDatePicker materialDatePicker = builder.build();
 
@@ -36,11 +27,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
-            @Override
-            public void onPositiveButtonClick(Object selection){
-                mSelectedDateText.setText("Selected Date: " + materialDatePicker.getHeaderText());
-            }
-        });
+
     }
 }
