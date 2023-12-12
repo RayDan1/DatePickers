bf8c0dd5f2b7c1c29765ee4e7ad2d538852a4710
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-12-11 23:14:31 -0700

    3ra. forma...

--- app/src/main/java/com/example/datepickers/MainActivity.java
+++ app/src/main/java/com/example/datepickers/MainActivity.java
@@ -1,23 +1,48 @@
 package com.example.datepickers;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.util.Pair;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.MaterialDatePicker;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class MainActivity extends AppCompatActivity {
+
     private Button mDatePickerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mDatePickerBtn = findViewById(R.id.date_picker_btn);
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+
+        calendar.roll(Calendar.MONTH, Calendar.JANUARY);
+        long january = calendar.getTimeInMillis();
+
+        calendar.roll(Calendar.MONTH, Calendar.DECEMBER);
+        long december = calendar.getTimeInMillis();
+
+        //CalendarConstraints
+        CalendarConstraints.Builder constraintBuilder = new CalendarConstraints.Builder();
+        constraintBuilder.setStart(january);
+        constraintBuilder.setEnd(december);
+
         //MaterialDatePicker
-        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
         builder.setTitleText("SELECT A DATE");
+        builder.setSelection(today);
+        builder.setCalendarConstraints(constraintBuilder.build());
         final MaterialDatePicker materialDatePicker = builder.build();
 
         mDatePickerBtn.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +51,5 @@ public class MainActivity extends AppCompatActivity {
                 materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
             }
         });
-
-
     }
 }
